cb3e6594f57cca24cf3285d065e29a1dedb34a8d
Ronan Mulligan <102162036+RonanMM@users.noreply.github.com>
2022-10-20 19:09:14 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -3,4 +3,19 @@
 
 def primes(number_of_primes):
     list = []
+    counter = 2
+    while len(list) <number_of_primes :
+        verification = False
+        print(list)
+        for div in range(2,counter):
+            
+            if counter % div == 0:
+                verification = True
+            div += 1
+            
+        if verification == False:
+            list.append(counter)
+        counter += 1
     return list
+
+print(primes(10))
