892a2e452ac93cf7ae37a41d30b3d3c4a2b1b574
Ronan Mulligan <102162036+RonanMM@users.noreply.github.com>
2022-10-24 00:00:58 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -2,6 +2,9 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if check(number_of_primes) == False:
+        return None
+    
     list = []
     counter = 2
     while len(list) <number_of_primes :
@@ -17,5 +20,11 @@ def primes(number_of_primes):
             list.append(counter)
         counter += 1
     return list
+
+def check(number_of_primes):
+    if number_of_primes < 1:
+        return False
+    else:
+        return True
 
 print(primes(10))
